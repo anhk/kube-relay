ae10f5c1d4c71780197548741b484ed895df2034
anhongkui <anhongkui@jd.com>
2024-02-27 11:36:19 +0800

    注释

--- cmd/kube-relay/app.go
+++ cmd/kube-relay/app.go
@@ -16,5 +16,11 @@ func (app *App) Run(option *Option) error {
 		gvr := processResource(resource)
 		log.Info("resource=%v, group=%v, version=%v", gvr.Resource, gvr.Group, gvr.Version)
 	}
+	// Step 1# 检查所有资源都存在
+
+	// Step 2# 启动Informer，等待Cache完成
+
+	// Step 3# 启动侦听
+
 	return nil
 }
